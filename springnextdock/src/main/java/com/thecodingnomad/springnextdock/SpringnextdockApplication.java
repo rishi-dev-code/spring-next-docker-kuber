b15beb91b666167cc858011f85e2de38c70cdb17
Rishi <email4rishi@fastmail.com>
2023-08-10 00:26:00 +1000

    added spring API initial project

---
+++ springnextdock/src/main/java/com/thecodingnomad/springnextdock/SpringnextdockApplication.java
@@ -0,0 +1,13 @@
+package com.thecodingnomad.springnextdock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringnextdockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringnextdockApplication.class, args);
+	}
+
+}
---
+++ springnextdock/src/test/java/com/thecodingnomad/springnextdock/SpringnextdockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecodingnomad.springnextdock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringnextdockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
